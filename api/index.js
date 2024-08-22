9486e55af4d2b949e4815bd69502f913bb97db2f
Nihar Popat <niharpopat17@gmail.com>
2024-08-22 18:06:19 +0530

    MiddleWare for errors

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 import authRouter from'./routes/auth.route.js';
 
-dotenv.config();
+dotenv.config(); 
 
 mongoose.connect(process.env.MONGO).then(() => {
   console.log("Connected")
@@ -20,3 +20,14 @@ app.listen(3000, () => {
 
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);   
+
+//middleware to handle errors
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500; //500 : Internal Server Error
+  const message = err.message || 'Internal Server Error';
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
